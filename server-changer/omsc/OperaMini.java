ea4a191f8262f7b5a8555f3f8fc66d32fa48d182
muzuiget <muzuiget@7a5bb8be-d8ea-11de-b590-7f2b5bfbe1fd>
2010-07-23 10:10:22 +0000

    加入版本自动检测

--- server-changer/omsc/OperaMini.java
+++ server-changer/omsc/OperaMini.java
@@ -4,7 +4,6 @@ public class OperaMini {
 
 	public static final OperaMini international, internationalNext;
 	public static final OperaMini china, chinaNext, chinaLab;
-	public static final OperaMini[] operaMiniItems;
 	public static final String SERVER_KEY = "c1dd7ab77e2c967746fe10681026c920f864811321bcb8be6bbfa5a03fda4e16c9c8db3af280f7703366e778e93c55e7159a8852d2b1381e521a337f22b1406cddf41a3114aecb4f4bfe79e0c5aa2ba8824fc989cb8bdcbf8ec5cef5176bfd4059f229b91bfa025126b295f9c409e75f6f6415ee094fd7f5dfd395a1f431668c5a08e88de891dc4dd38d4e9aa9b9c00dc604a0428e3aa5a28ccfa75af099147b";
 	public static final String CHINA_SERVER_KEY = "8c60d2a6811f85366af231ae416831b09409b614e9cfa8fde8d8577e892636e0e0b7a151f9601b930bf527ea8a22bfe6fb5f72506bd3e81b3b55d189af17e35b2d7ea61d84ba4e62cf1c01789edb2c3f3c00fc3c09ee1fc9627367294727e52af4c990516d8d7aad4e00d6ab50cd8ca63705df0af243e666dad282d6514b656780e108d591cf78920f7bdee21ed1419a080655ca2acdadc4e64dba01b5accf73";
 
@@ -13,12 +12,10 @@ public class OperaMini {
 	public boolean changerKey;
 
 	public OperaMini(String name, String version, String downloadLink,
-			String classFile, String httpServer, String socketServer,
-			boolean changerKey) {
+			String httpServer, String socketServer, boolean changerKey) {
 		this.name = name;
 		this.version = version;
 		this.downloadLink = downloadLink;
-		this.classFile = classFile;
 		this.httpServer = httpServer;
 		this.socketServer = socketServer;
 		this.changerKey = changerKey;
@@ -29,38 +26,32 @@ public class OperaMini {
 		return text;
 	}
 
-	public String toString() {
-		return String.format("Opera Mini %s %s", version, name);
+	public void setClassFile(String filename) {
+		this.classFile = filename;
 	}
 
 	static {
 		international = new OperaMini("国际版", "4.2",
 				"http://www.opera.com/mobile/download/versions/",
-				"h.class", "http://server4.operamini.com:80/",
+				"http://server4.operamini.com:80/",
 				"socket://server4.operamini.com:1080", false);
 		internationalNext = new OperaMini("国际版", "5.1",
-				"http://www.opera.com/mobile/download/versions/", "o.class",
+				"http://www.opera.com/mobile/download/versions/",
 				"http://mini5.opera-mini.net:80/",
 				"socket://mini5.opera-mini.net:1080", false);
 		china = new OperaMini("中国版", "4.2",
-				"http://www.operachina.com/mini/download/", "g.class",
+				"http://www.operachina.com/mini/download/",
 				"http://59.151.106.229:80/", "socket://59.151.106.229:1080",
 				true);
 		chinaNext = new OperaMini("中国版", "5.1",
-				"http://www.operachina.com/mini/download/", "o.class",
+				"http://www.operachina.com/mini/download/",
 				"http://mini5cn.opera-mini.net:80/",
 				"socket://mini5cn.opera-mini.net:1080", true);
 		chinaLab = new OperaMini("实验室版", "4.2",
-				"http://www.operachina.com/id/", "I.class",
+				"http://www.operachina.com/id/",
 				"http://china-4.opera-mini.net:80/",
 				"socket://china-4.opera-mini.net:1080", true);
 
-		operaMiniItems = new OperaMini[5];
-		operaMiniItems[0] = international;
-		operaMiniItems[1] = internationalNext;
-		operaMiniItems[2] = china;
-		operaMiniItems[3] = chinaNext;
-		operaMiniItems[4] = chinaLab;
 	}
 
 }
